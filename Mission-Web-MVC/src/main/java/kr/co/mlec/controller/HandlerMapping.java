8f93efa7dc6bba7f85eb207557ba17604bb25898
DESKTOP-G7FUHM5\user <iamjiyoo.kopo@gmail.com>
2022-01-19 16:12:26 +0900

    0119-오후

--- Mission-Web-MVC/src/main/java/kr/co/mlec/controller/HandlerMapping.java
+++ Mission-Web-MVC/src/main/java/kr/co/mlec/controller/HandlerMapping.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 
 public class HandlerMapping {
 
@@ -13,22 +14,54 @@ public class HandlerMapping {
 	public HandlerMapping() {
 		
 		Properties prop = new Properties();
+		mappings = new HashMap<>();
+		
 		try {
 			InputStream is = new FileInputStream("D:\\Lecutre\\web-workspace\\Mission-Web-MVC\\bean.properties");
 			prop.load(is);
+	
+			Set<Object> keys = prop.keySet();
+			for(Object key : keys) {
+				String className = prop.getProperty(key.toString());
+				System.out.println(key + " : " + className);
+				
+				Class<?> clz = Class.forName(className);
+				mappings.put(key.toString(), (Controller)clz.newInstance());
+			
+			}
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 		
-		mappings = new HashMap<>();
-		mappings.put("/board/list.do", new BoardListController());
-		mappings.put("/board/writeForm.do", new BoardWriteFormController());
+		
 	}
 	
 	public Controller getController(String uri) {
 		return mappings.get(uri);
 	}
-
+/*	
+	public static void main(String[] args) throws Exception {
+		
+		
+//		java.util.Random r = new java.util.Random();
+//		int random = r.nextInt(100);
+//		System.out.println("추출된 난수 : " + random);
+		
+		
+		
+//		Class<?> clz = Class.forName("java.util.Random");
+//		java.util.Random obj = (java.util.Random)clz.newInstance();
+//		int random = obj.nextInt(100);
+//		System.out.println("추출된 난수 : " + random);
+		
+		
+		// BoardListController obj = new BoardListController();
+		Class<?> clz = Class.forName("kr.co.mlec.controller.BoardListController");
+		BoardListController obj = (BoardListController)clz.newInstance();
+		System.out.println(obj.handleRequest(null, null));
+	}
+*/
 }
 
 
